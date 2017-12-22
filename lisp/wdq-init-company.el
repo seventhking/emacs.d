ef6f35e1d97a3497013702293f42a2e4fffdaa26
seventhking <deetchwang@163.com>
2017-12-22 17:54:23 +0800

    init

--- lisp/wdq-init-company.el
+++ lisp/wdq-init-company.el
@@ -1,7 +1,7 @@
 ;;[wdq] configuration start
 
 ;;anaconda-mode
-(add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'python-mode-hook 'anaconda-mode)
 (after-load 'company
   (add-hook 'python-mode-hook
             (lambda () (sanityinc/local-push-company-backend #'company-anaconda))))
@@ -9,15 +9,20 @@
 ;;company-lua
 (after-load 'company
   (add-hook 'lua-mode-hook
-            (lambda () (sanityinc/local-push-company-backend #'company-lua))))
+            (lambda () (sanityinc/local-push-company-backend #'company-lua)))
+  (add-hook 'lua-mode-hook
+            (lambda () (sanityinc/local-push-company-backend #'company-gtags))))
 
 
 ;;company-c-headers
+;;company-irony
 (require 'company-c-headers)
 
 (after-load 'company
   (add-hook 'c-mode-common-hook
-            (lambda () (sanityinc/local-push-company-backend #'company-c-headers))))
+            (lambda () (sanityinc/local-push-company-backend #'company-c-headers)))
+  (add-hook 'c-mode-common-hook
+            (lambda () (sanityinc/local-push-company-backend #'company-irony))))
 
 (add-to-list 'company-c-headers-path-system '"/usr/include/c++/4.8.2/")
 (add-to-list 'company-c-headers-path-system '"~/mylib/c-include")
@@ -28,14 +33,34 @@
 ;;   (add-hook 'c-mode-common-hook
 ;;             (lambda () (sanityinc/local-push-company-backend #'company-clang))))
 
-;;company-irony
-(after-load 'company
-  (add-hook 'c-mode-common-hook
-            (lambda () (sanityinc/local-push-company-backend #'company-irony))))
 
 ;;(setq company-backends (delete 'company-semantic company-backends))
 
 ;;(setq company-clang-arguments '("-I/usr/include" "-I/usr/include/c++/4.8.2"))
+
+(defun company-diag ()
+  (interactive)
+  (let* ((bb company-backends)
+         backend
+         (prefix (cl-loop for b in bb
+                          thereis (let ((company-backend b))
+                                    (setq backend b)
+                                    (company-call-backend 'prefix))))
+         cc)
+    (when (stringp prefix)
+      (setq cc (let ((company-backend backend))
+                 (company-call-backend 'candidates prefix))))
+    (pop-to-buffer (get-buffer-create "*company-diag*"))
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (insert "company-backends: " (pp-to-string bb))
+    (insert "\n")
+    (insert "Used backend: " (pp-to-string backend))
+    (insert "\n")
+    (insert "Prefix: " (pp-to-string prefix))
+    (insert "\n")
+    (insert (message  "Candidates number: %i" (length cc)))
+    (special-mode)))
 
 (provide 'wdq-init-company)
 
