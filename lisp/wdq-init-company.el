7061df350e75dba42d83ee8ffe256b33ba336bae
seventh <deetchwang@163.com>
2018-06-14 00:08:07 +0800

    use company-irony-c-headers instead of company-c-headers

--- lisp/wdq-init-company.el
+++ lisp/wdq-init-company.el
@@ -22,18 +22,20 @@
 ;;company-irony
 (require-package 'company-c-headers)
 (require-package 'company-irony)
+(require-package 'company-irony-c-headers)
 
 (after-load 'company
   (add-hook 'c-mode-common-hook
-            (lambda () (sanityinc/local-push-company-backend #'company-c-headers)))
+            (lambda () (sanityinc/local-push-company-backend #'company-irony-c-headers)))
+  ;; (add-hook 'c-mode-common-hook
+  ;;           (lambda () (sanityinc/local-push-company-backend #'company-c-headers)))
   (add-hook 'c-mode-common-hook
             (lambda () (sanityinc/local-push-company-backend #'company-irony))))
 
-(require 'company-c-headers)
-
-(add-to-list 'company-c-headers-path-system '"/usr/include/c++/7.2.0")
-(add-to-list 'company-c-headers-path-system '"~/mylib/c-include")
-(add-to-list 'company-c-headers-path-system '"~/mylib/cpp-include")
+;; (require 'company-c-headers)
+;; (add-to-list 'company-c-headers-path-system '"/usr/include/c++/7.3.0")
+;; (add-to-list 'company-c-headers-path-system '"~/mylib/c-include")
+;; (add-to-list 'company-c-headers-path-system '"~/mylib/cpp-include")
 
 ;;company-clang
 ;; (after-load 'company
