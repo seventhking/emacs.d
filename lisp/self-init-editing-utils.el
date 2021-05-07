518664eceb7b4466c5bffc31828bc82a96f73821
deetch <deetch>
2021-05-07 13:50:06 +0800

    init

--- lisp/self-init-editing-utils.el
+++ lisp/self-init-editing-utils.el
@@ -1,7 +1,35 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 使用undo-tree
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require-package 'undo-tree)
 (add-hook 'after-init-hook 'global-undo-tree-mode)
 (after-load 'undo-tree
   (diminish 'undo-tree-mode))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 在selectrum-mode下，DEL键设置为跳转到上级目录
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (defun selectrum-up-directory ()
+;;   (interactive)
+;;   (let ((directory (expand-file-name (minibuffer-contents-no-properties))))
+;;     (delete-minibuffer-contents)
+;;     (insert (string-trim-right directory "[^/]+/?"))))
+;;; 或这样
+(defun selectrum-up-directory ()
+  "回到上层目录"
+  (interactive)
+  (let ((directory (minibuffer-contents-no-properties)))
+    (delete-minibuffer-contents)
+    (insert (string-trim-right (if (string= directory "~/") (expand-file-name "~/") directory) "[^/]+/?"))))
+
+(define-key minibuffer-local-map (kbd "DEL") #'selectrum-up-directory)
+
 
 
 (provide 'self-init-editing-utils)
