dc14e26ec3d8798681efc55ee8b5dc32082588b2
deetch <deetch>
2021-05-13 13:50:22 +0800

    init

--- lisp/self-init-common.el
+++ lisp/self-init-common.el
@@ -18,10 +18,5 @@
 (define-coding-system-alias 'UTF-8 'utf-8)
 (setq debug-on-error nil)
 
-(when (maybe-require-package 'moom)
-  (with-eval-after-load "moom"
-    (moom-mode 1)))
-
-
 
 (provide 'self-init-common)
